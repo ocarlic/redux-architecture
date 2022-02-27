8e1a825abcb41539ea13f024e8d9d0a6b7b22357
Victor Carli <victorcarlibr@gmail.com>
2022-02-27 20:05:17 -0300

    feat: listing products

--- src/components/catalog.tsx
+++ src/components/catalog.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import api from '../services/api'
+import { IProduct } from '../store/modules/cart/types'
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state)
+  const [catalog, setCatalog] = useState<IProduct[]>([])
 
-  console.log('catalog:', catalog)
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
 
-  return <div>Catalog</div>
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {" "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  )
 }
 
 export default Catalog
