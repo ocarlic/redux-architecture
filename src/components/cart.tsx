9491b03a4d1d12c966085b65a4323b271619aa4a
Victor Carli <victorcarlibr@gmail.com>
2022-02-27 20:43:46 -0300

    feat: creating cart types

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { IState } from '../store'
+import { ICartItem } from '../store/modules/cart/types'
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state)
-
-  console.log('Cart: ', state)
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   return (
     <table>
@@ -17,7 +17,14 @@ const Cart: React.FC = () => {
         </tr>
       </thead>
       <tbody>
-
+        {cart.map(item => (
+          <tr key={item.product.id}>
+            <th>{item.product.title}</th>
+            <th>{item.product.price}</th>
+            <th>{item.quantity}</th>
+            <th>{(item.product.price * item.quantity).toFixed(2)}</th>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
