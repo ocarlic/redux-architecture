5d174e021ebd533097a7115f399c0d74248d74a3
Victor Carli <victorcarlibr@gmail.com>
2022-02-27 20:56:04 -0300

    feat: added immer and increasing the amount

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from 'immer'
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,25 +7,31 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload
+  return produce(state, draft => {
+    switch(action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1
+          const productInCartIndex = draft.items.findIndex(item => (
+            item.product.id === product.id
+          ))
+
+          if (productInCartIndex >= 0) {
+            draft.items[productInCartIndex].quantity++
+          } else {
+            draft.items.push({
+              product,
+              quantity: 1
+            })
           }
-        ]
+
+        break
+      }
+      default: {
+        return state
       }
     }
-    default: {
-      return state
-    }
-  }
+  })
 }
 
 export default cart;
